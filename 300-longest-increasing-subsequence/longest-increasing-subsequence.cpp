092770ebfab78137b199be4418a4647396ee83f2
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-02-19 14:56:54 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (78.93%) - LeetSync

---
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> LIS;
+
+        for(int i = 0; i < nums.size(); i++) {
+            if(LIS.empty() || LIS.back() < nums[i]) {
+                LIS.push_back(nums[i]);
+            } 
+            else {
+                int idx = lower_bound(LIS.begin(), LIS.end(), nums[i]) - LIS.begin();
+                LIS[idx] = nums[i];
+            }
+        }
+        return LIS.size();
+    }
+};
