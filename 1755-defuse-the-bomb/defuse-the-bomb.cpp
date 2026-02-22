58b189c00e8d7a23031d69952d025c42fdfa10c6
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-02-22 19:53:06 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (50.86%) - LeetSync

---
+++ 1755-defuse-the-bomb/defuse-the-bomb.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> decrypt(vector<int>& code, int k) {
+        int n = code.size();
+        vector<int> result(n);
+        if(k == 0)
+         return result;
+
+        for(int i = 0; i < n; i++){
+            int sum = 0;
+
+            if(k > 0){
+                for(int j = 1; j <= k; j++){
+                    int index = (i + j) % n;
+                    sum += code[index];
+                }
+            }
+                else{
+                    for(int j = 1; j <= abs(k); j++) {
+                    int index = (i - j + n) % n;
+                    sum += code[index];
+                }
+            }
+            result[i] = sum;
+        }
+        return result;
+    }
+};
