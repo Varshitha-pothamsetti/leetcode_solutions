48292da1e679bd465fe9fc0973974fb5eb567f02
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-02-25 20:40:06 +0530

    Time: 34 ms (41.86%) | Memory: 52.1 MB (22.04%) - LeetSync

---
+++ 924-fair-candy-swap/fair-candy-swap.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> fairCandySwap(vector<int>& aliceSizes, vector<int>& bobSizes) {
+        
+        int suma = 0, sumb = 0;
+
+        for(int a : aliceSizes)
+            suma += a; 
+        
+        for(int b : bobSizes)
+            sumb += b;
+            
+        int diff = (suma - sumb) / 2;
+
+        unordered_set<int> bobSet(bobSizes.begin(), bobSizes.end());
+
+        for(int x : aliceSizes){
+            int target = x - diff;
+
+            if(bobSet.count(target)){
+                return {x, target};
+            }
+        }
+
+        return {}; 
+    }
+};
