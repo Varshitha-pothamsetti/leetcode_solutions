6717a7507f63b97d4e55b26a904f9efa07f47e9d
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-03-01 20:19:13 +0530

    Create 3541-ReportSpamMessage

---
+++ 3541-ReportSpamMessage/3541-ReportSpamMessage.py
@@ -0,0 +1,13 @@
+# Last updated: 01/03/2026, 20:19:11
+class Solution:
+    def reportSpam(self, message: List[str], bannedWords: List[str]) -> bool:
+        banned_set = set(bannedWords)
+        count = 0
+        for word in message:
+            if word in banned_set:
+                count += 1
+                if count >= 2:
+                    return True
+        return False
+
+        
