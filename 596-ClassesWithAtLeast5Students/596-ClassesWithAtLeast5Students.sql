a2ec926a6844b91eb61e7a91c39d17cf39bd24dd
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-03-01 20:21:37 +0530

    Create 596-ClassesWithAtLeast5Students

---
+++ 596-ClassesWithAtLeast5Students/596-ClassesWithAtLeast5Students.sql
@@ -0,0 +1,9 @@
+-- Last updated: 01/03/2026, 20:21:35
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+with method as(
+    select class , count(class) as county
+    from courses 
+    group by class
+)
+select class from method where county>=5;
