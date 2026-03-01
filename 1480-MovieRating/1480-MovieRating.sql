4c673e9c859540745499b049572f7d070964619d
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-03-01 20:20:39 +0530

    Create 1480-MovieRating

---
+++ 1480-MovieRating/1480-MovieRating.sql
@@ -0,0 +1,20 @@
+-- Last updated: 01/03/2026, 20:20:37
+# Write your MySQL query statement below
+(
+    SELECT u.name AS results
+    FROM MovieRating mr
+    JOIN Users u ON mr.user_id = u.user_id
+    GROUP BY u.user_id, u.name
+    ORDER BY COUNT(DISTINCT mr.movie_id) DESC, u.name ASC
+    LIMIT 1
+)
+UNION ALL
+(
+    SELECT m.title AS results
+    FROM MovieRating mr
+    JOIN Movies m ON mr.movie_id = m.movie_id
+    WHERE mr.created_at BETWEEN '2020-02-01' AND '2020-02-29'
+    GROUP BY m.movie_id, m.title
+    ORDER BY AVG(mr.rating) DESC, m.title ASC
+    LIMIT 1
+);
