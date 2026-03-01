bcc09f566f1c02d607780d3ad7b93b7c41b5a29b
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-03-01 20:19:23 +0530

    Create 3371-HarshadNumber

---
+++ 3371-HarshadNumber/3371-HarshadNumber.cpp
@@ -0,0 +1,16 @@
+// Last updated: 01/03/2026, 20:19:21
+class Solution {
+public:
+    int sumOfTheDigitsOfHarshadNumber(int x) {
+        int sum = 0;
+        int temp = x;
+        while(temp > 0){
+            sum += (temp % 10);
+            temp /= 10;
+        }
+        if(x % sum == 0){
+            return sum;
+        }
+        return -1;
+    }
+};
