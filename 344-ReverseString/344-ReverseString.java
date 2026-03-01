f460c93cc2f4a138a8fc7a0c874cb13539d149a2
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-03-01 20:22:03 +0530

    Create 344-ReverseString

---
+++ 344-ReverseString/344-ReverseString.java
@@ -0,0 +1,11 @@
+// Last updated: 01/03/2026, 20:22:01
+class Solution {
+    public void reverseString(char[] s) {
+        int n = s.length;
+        for(int i=0;i<n/2;i++){
+            char ch = s[i];
+            s[i] = s[n-i-1];
+            s[n-i-1] = ch;
+        }   
+    }
+}
