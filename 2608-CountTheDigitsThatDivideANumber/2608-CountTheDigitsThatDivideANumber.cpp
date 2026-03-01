ce4b070f634282da6948db9f2d7bdba49bb4dee5
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-03-01 20:19:43 +0530

    Create 2608-CountTheDigitsThatDivideANumber

---
+++ 2608-CountTheDigitsThatDivideANumber/2608-CountTheDigitsThatDivideANumber.cpp
@@ -0,0 +1,18 @@
+// Last updated: 01/03/2026, 20:19:41
+class Solution {
+public:
+    int countDigits(int num) {
+        int count = 0;
+        int temp = num;
+        while(temp != 0){
+            int val = temp % 10;
+            if(val != 0){
+                if(num % val == 0){
+                    count++;
+                }
+            }
+            temp /= 10;
+        }
+        return count;
+    }
+};
