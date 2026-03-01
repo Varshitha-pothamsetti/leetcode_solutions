eaa66fd6cd17ce857938dd20f89658634e355715
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-03-01 20:23:03 +0530

    Create 83-RemoveDuplicatesFromSortedList

---
+++ 83-RemoveDuplicatesFromSortedList/83-RemoveDuplicatesFromSortedList.c
@@ -0,0 +1,21 @@
+// Last updated: 01/03/2026, 20:23:01
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* deleteDuplicates(struct ListNode* head) {
+    struct ListNode* current = head;
+    while (current != NULL && current->next != NULL) {
+        if(current->val == current->next->val){
+            struct ListNode* temp = current->next;
+            current->next = current->next->next;
+            free(temp);
+        }else{
+            current = current->next;
+        }
+    }
+    return head; 
+}
