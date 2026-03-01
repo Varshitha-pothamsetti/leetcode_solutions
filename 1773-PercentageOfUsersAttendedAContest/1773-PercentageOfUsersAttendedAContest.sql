4469efa0e8be1e5f3b484aead57ac87bf8b13466
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-03-01 20:20:20 +0530

    Create 1773-PercentageOfUsersAttendedAContest

---
+++ 1773-PercentageOfUsersAttendedAContest/1773-PercentageOfUsersAttendedAContest.sql
@@ -0,0 +1,7 @@
+-- Last updated: 01/03/2026, 20:20:18
+# Write your MySQL query statement below
+select r.contest_id,
+round(count(r.user_id) * 100.0 / (select count(*) from users), 2) as percentage
+from register r
+group by r.contest_id
+order by percentage desc, contest_id asc;
