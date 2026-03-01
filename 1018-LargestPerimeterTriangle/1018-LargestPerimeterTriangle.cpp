19fe2ef306ede55af7bc4d66884c0f6110463725
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-03-01 20:21:06 +0530

    Create 1018-LargestPerimeterTriangle

---
+++ 1018-LargestPerimeterTriangle/1018-LargestPerimeterTriangle.cpp
@@ -0,0 +1,16 @@
+// Last updated: 01/03/2026, 20:21:04
+class Solution {
+public:
+    int largestPerimeter(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n =nums.size();
+        for(int i = n - 1; i >= 2 ; i--){
+            int a = nums[i], b = nums[i - 1], c = nums[i - 2];
+            if(b + c > a){
+                return a + b + c;
+            }
+        }
+        return 0;
+
+    }
+};
