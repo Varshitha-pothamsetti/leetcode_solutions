8a1afbd296c7fb8dcf155f108514ca8516e39ee4
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-03-01 20:22:28 +0530

    Create 206-ReverseLinkedList

---
+++ 206-ReverseLinkedList/206-ReverseLinkedList.cpp
@@ -0,0 +1,25 @@
+// Last updated: 01/03/2026, 20:22:26
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        while(curr != nullptr){
+            ListNode* temp = curr->next;
+            curr -> next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+    }
+};
