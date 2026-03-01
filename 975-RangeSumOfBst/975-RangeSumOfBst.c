bf9d9ed4d3b0ccab7479f32a7ea3211c8144709e
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-03-01 20:21:08 +0530

    Create 975-RangeSumOfBst

---
+++ 975-RangeSumOfBst/975-RangeSumOfBst.c
@@ -0,0 +1,22 @@
+// Last updated: 01/03/2026, 20:21:06
+int rangeSumBST(struct TreeNode* root, int low, int high) {
+    struct TreeNode *stack[10000], *node;
+    int sum = 0, sp = 0, val;
+    if (root != NULL) {
+        stack[sp++] = root;
+    }
+    while (sp > 0) {
+        node = stack[--sp];
+        val = node->val;
+        if (val >= low && val <= high) {
+            sum += node->val;
+        }
+        if (val <= high && node->right != NULL) {
+            stack[sp++] = node->right;
+        }
+        if (val >= low && node->left != NULL) {
+            stack[sp++] = node->left;
+        }
+    }
+    return sum;  
+}
