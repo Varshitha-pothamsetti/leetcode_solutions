0a98517f2402c6c11ec3289956eec1a395ce0cba
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-03-01 20:22:07 +0530

    Create 303-RangeSumQueryImmutable

---
+++ 303-RangeSumQueryImmutable/303-RangeSumQueryImmutable.cpp
@@ -0,0 +1,25 @@
+// Last updated: 01/03/2026, 20:22:05
+class NumArray {
+private:
+vector<int> prefix;
+
+public:
+    NumArray(vector<int>& nums) {
+        prefix.push_back(0);
+
+        for(int num : nums) {
+            prefix.push_back(prefix.back() + num);
+        }
+        
+    }
+    
+    int sumRange(int left, int right) {
+        return prefix[right + 1] - prefix[left];
+    }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * int param_1 = obj->sumRange(left,right);
+ */
