9bec542ac05288de69455c4817a185432b52e82e
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-03-01 20:18:51 +0530

    Create 4194-CountMonobitIntegers

---
+++ 4194-CountMonobitIntegers/4194-CountMonobitIntegers.cpp
@@ -0,0 +1,14 @@
+// Last updated: 01/03/2026, 20:18:50
+class Solution {
+public:
+    int countMonobit(int n) {
+        int count = 1;
+        int val = 1;
+
+        while(val <= n){
+            count++;
+            val = (val << 1) | 1;
+        }
+        return count;
+    }
+};
