e7892766cbc9c37f4de994d4a1eacba7f4985ef8
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-03-01 20:19:11 +0530

    Create 3567-ConvertDateToBinary

---
+++ 3567-ConvertDateToBinary/3567-ConvertDateToBinary.java
@@ -0,0 +1,15 @@
+// Last updated: 01/03/2026, 20:19:09
+class Solution {
+    public String convertDateToBinary(String date) {
+        String y = date.substring(0,4);
+        String m = date.substring(5,7);
+        String d = date.substring(8,10);
+        StringBuilder sb = new StringBuilder();
+        sb.append(Integer.toBinaryString(Integer.parseInt(y)));
+        sb.append("-");
+        sb.append(Integer.toBinaryString(Integer.parseInt(m)));
+        sb.append("-");
+        sb.append(Integer.toBinaryString(Integer.parseInt(d)));
+        return sb.toString();
+    }
+}
