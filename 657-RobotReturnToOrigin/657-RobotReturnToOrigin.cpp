093db9541983e5331d9b7f2863c5e584af793dcb
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-03-01 20:21:24 +0530

    Create 657-RobotReturnToOrigin

---
+++ 657-RobotReturnToOrigin/657-RobotReturnToOrigin.cpp
@@ -0,0 +1,25 @@
+// Last updated: 01/03/2026, 20:21:22
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x = 0, y = 0;
+        for(int i = 0; i < moves.size(); i++){
+            if(moves[i] == 'R'){
+                x++;
+            }
+            else if(moves[i] == 'L'){
+                x--;
+            }
+            else if(moves[i] == 'U'){
+                y++;
+            }
+            else if(moves[i] == 'D'){
+                y--;
+            }
+        }
+        if(x == 0 && y == 0){
+            return true;
+        }
+        else return false;
+    }
+};
