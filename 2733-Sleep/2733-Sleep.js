c93115a29520bbbabb8360e643d61b0f0991de33
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-03-01 20:19:37 +0530

    Create 2733-Sleep

---
+++ 2733-Sleep/2733-Sleep.js
@@ -0,0 +1,13 @@
+// Last updated: 01/03/2026, 20:19:35
+/**
+ * @param {number} millis
+ * @return {Promise}
+ */
+async function sleep(millis) {
+    return new Promise(resolve => setTimeout(resolve, millis));
+}
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
