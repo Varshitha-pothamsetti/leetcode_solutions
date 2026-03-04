3f721b5fb00d44c7492c5c69f86347c7af268e17
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-03-04 13:47:03 +0530

    Create 389-FindtheDifference

---
+++ 389-FindtheDifference/389-FindtheDifference.cpp
@@ -0,0 +1,20 @@
+// Last updated: 04/03/2026, 13:47:02
+1class Solution {
+2public:
+3    char findTheDifference(string s, string t) {
+4      char c;
+5      int arr[26] = {0};
+6      for(int i = 0; i < s.length(); i++){
+7        arr[s[i] - 'a']++;
+8      }  
+9      for(int i = 0; i < t.length(); i++){
+10        if(arr[t[i] - 'a'] > 0){
+11            arr[t[i] - 'a']--;
+12        }
+13        else if(arr[t[i] - 'a'] == 0){
+14            c = t[i];
+15        }
+16      }
+17      return c;
+18    }
+19};
