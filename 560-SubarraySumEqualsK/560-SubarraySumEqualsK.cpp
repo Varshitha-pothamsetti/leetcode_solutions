3e11546fca41a177243219174a551c6b6c083b45
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-03-05 10:31:56 +0530

    File updated at 05/03/2026, 10:31:56

--- 560-SubarraySumEqualsK/560-SubarraySumEqualsK.cpp
+++ 560-SubarraySumEqualsK/560-SubarraySumEqualsK.cpp
@@ -1,20 +1,20 @@
-// Last updated: 01/03/2026, 20:21:45
-class Solution {
-public:
-    int subarraySum(vector<int>& nums, int k) {
-        int count = 0;
-        int prefix = 0;
-        unordered_map<int, int> map;
-        map[0]++;
-
-        for(int num : nums){
-            prefix += num;
-            int need = prefix - k;
-            if(map.find(need) != map.end()){
-                count += map[need];
-            }
-            map[prefix]++;
-        }  
-        return count;
-    }
-};
+// Last updated: 05/03/2026, 10:31:56
+1class Solution {
+2public:
+3    int subarraySum(vector<int>& nums, int k) {
+4        int count = 0;
+5        int prefix = 0;
+6        unordered_map<int, int> map;
+7        map[0]++;
+8
+9        for(int num : nums){
+10            prefix += num;
+11            int need = prefix - k;
+12            if(map.find(need) != map.end()){
+13                count += map[need];
+14            }
+15            map[prefix]++;
+16        }  
+17        return count;
+18    }
+19};
