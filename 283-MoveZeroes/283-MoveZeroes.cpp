e0a673f2e3bb6f711ff54ac47e4a8b9966a6e1f5
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-03-05 10:37:10 +0530

    File updated at 05/03/2026, 10:37:09

--- 283-MoveZeroes/283-MoveZeroes.cpp
+++ 283-MoveZeroes/283-MoveZeroes.cpp
@@ -1,13 +1,13 @@
-// Last updated: 01/03/2026, 20:22:12
-class Solution {
-public:
-    void moveZeroes(vector<int>& nums) {
-        int i = 0, n = 0;
-        while(i < nums.size()){
-            if(nums[i] != 0) swap(nums[i], nums[n++]);
-            i++;
-        }
-    }
-};
-
-
+// Last updated: 05/03/2026, 10:37:09
+1class Solution {
+2public:
+3    void moveZeroes(vector<int>& nums) {
+4        int i = 0, n = 0;
+5        while(i < nums.size()){
+6            if(nums[i] != 0) swap(nums[i], nums[n++]);
+7            i++;
+8        }
+9    }
+10};
+11
+12
