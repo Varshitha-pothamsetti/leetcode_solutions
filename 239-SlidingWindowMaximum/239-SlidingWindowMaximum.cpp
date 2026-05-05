ba2631a369b635430c335a2f788f0e1d26f880ca
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-05-05 21:51:44 +0530

    File updated at 05/05/2026, 21:51:43

--- 239-SlidingWindowMaximum/239-SlidingWindowMaximum.cpp
+++ 239-SlidingWindowMaximum/239-SlidingWindowMaximum.cpp
@@ -1,23 +1,24 @@
-// Last updated: 01/03/2026, 20:22:19
-#include <bits/stdc++.h>
-using namespace std;
-
-class Solution {
-public:
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        deque<int> dq;
-        vector<int> result;
-
-        for (int i = 0; i < nums.size(); i++) {
-            if (!dq.empty() && dq.front() == i - k)
-                dq.pop_front();
-            while (!dq.empty() && nums[dq.back()] < nums[i])
-                dq.pop_back();
-            dq.push_back(i);
-            if (i >= k - 1)
-                result.push_back(nums[dq.front()]);
-        }
-
-        return result;
-    }
-};
+// Last updated: 05/05/2026, 21:51:43
+1#include <bits/stdc++.h>
+2using namespace std;
+3
+4class Solution {
+5public:
+6    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+7        deque<int> dq;
+8        vector<int> result;
+9
+10        for (int i = 0; i < nums.size(); i++) {
+11            if (!dq.empty() && dq.front() == i - k)
+12                dq.pop_front();
+13            while (!dq.empty() && nums[dq.back()] < nums[i])
+14                dq.pop_back();
+15            dq.push_back(i);
+16            if (i >= k - 1)
+17                result.push_back(nums[dq.front()]);
+18        }
+19
+20        return result;
+21    }
+22};
+23
