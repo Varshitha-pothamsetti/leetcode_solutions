851dc9e42ac709dba46f6d3f938bcab1bb4cfa1f
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-05-13 21:15:23 +0530

    Create 2746-FilterElementsFromArray

---
+++ 2746-FilterElementsFromArray/2746-FilterElementsFromArray.js
@@ -0,0 +1,18 @@
+// Last updated: 13/05/2026, 21:15:23
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    let result = [];
+    for(let i=0;i<arr.length;i++){
+        if(fn(arr[i], i)){
+            result.push(arr[i]);
+        }
+    }
+    return result;
+};
+
+
+
