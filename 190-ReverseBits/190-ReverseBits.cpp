af305118f40effe2b5296b1f94aa1aacde715673
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-06-17 21:43:40 +0530

    File updated at 17/06/2026, 21:43:40

--- 190-ReverseBits/190-ReverseBits.cpp
+++ 190-ReverseBits/190-ReverseBits.cpp
@@ -1,21 +1,14 @@
-// Last updated: 29/05/2026, 12:56:24
+// Last updated: 17/06/2026, 21:43:40
 1class Solution {
 2public:
-3    int reverse(int x) {
-4        int rev = 0;
-5        while(x != 0){
-6            int digit = x % 10;
-7            x /= 10;
-8            if(rev > INT_MAX / 10 || 
-9            (rev == INT_MAX / 10 && digit > 7))
-10              return 0;
-11
-12            if(rev < INT_MIN / 10 || 
-13            (rev == INT_MIN / 10 && digit < -8))
-14             return 0;
-15            
-16            rev = rev * 10 + digit;
-17        }
-18        return rev;
-19    }
-20};
+3    int reverseBits(int n) {
+4        unsigned int num = (unsigned int)n;
+5        unsigned int ans = 0;
+6
+7        for(int i = 0; i < 32; i++){
+8            ans = (ans << 1) | (num & 1);
+9            num >>= 1;
+10        }
+11        return (int)ans;
+12    }
+13};
