dc37145da20cc2fd6d099d3fcb9c4e0277867848
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-06-18 20:47:58 +0530

    File updated at 18/06/2026, 20:47:58

--- 1927-SumGame/1927-SumGame.cpp
+++ 1927-SumGame/1927-SumGame.cpp
@@ -1,25 +1,25 @@
-// Last updated: 18/06/2026, 20:45:52
+// Last updated: 18/06/2026, 20:47:58
 1class Solution {
 2public:
 3    bool sumGame(string num) {
 4        int n = num.size();
-5        int sumL = 0, sumR = 0;
-6        int qL = 0, qR = 0;
+5        int sumLeft = 0, sumRight = 0;
+6        int qLeft = 0, qRight = 0;
 7        for(int i = 0; i < n/2; i++){
 8            if(num[i] == '?')
-9               qL++;
+9               qLeft++;
 10            else
-11               sumL += num[i] - '0';
+11               sumLeft += num[i] - '0';
 12        }
 13        for(int i = n/2; i < n; i++){
 14            if(num[i] == '?')
-15              qR++;
+15              qRight++;
 16            else
-17              sumR += num[i] - '0';
+17              sumRight += num[i] - '0';
 18        }
-19        if((qL + qR) % 2)
+19        if((qLeft + qRight) % 2)
 20           return true;
 21
-22           return(sumL - sumR) != (qR - qL) / 2 * 9;
+22           return(sumLeft - sumRight) != (qRight - qLeft) / 2 * 9;
 23    }
 24};
