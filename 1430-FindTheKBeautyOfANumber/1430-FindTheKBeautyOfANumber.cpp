2818205e48208f72902a3c02f7148c327b134000
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-06-24 13:31:39 +0530

    Create 1430-FindTheKBeautyOfANumber

---
+++ 1430-FindTheKBeautyOfANumber/1430-FindTheKBeautyOfANumber.cpp
@@ -0,0 +1,16 @@
+// Last updated: 24/06/2026, 13:31:38
+class Solution {
+public:
+    int divisorSubstrings(int num, int k) {
+        string s = to_string(num);
+        int count = 0;
+        for (int i = 0; i <= s.length() - k; i++) {
+            string sub = s.substr(i, k);
+            int val = stoi(sub);
+            if (val != 0 && num % val == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+};
