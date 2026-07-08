959feb28e212e50063be407d6ea6675a21ab70bc
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-07-08 21:15:59 +0530

    Create 118-PascalsTriangle

---
+++ 118-PascalsTriangle/118-PascalsTriangle.cpp
@@ -0,0 +1,20 @@
+// Last updated: 08/07/2026, 21:15:58
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+       vector<vector<int>> triangle;
+        for(int i = 0; i < numRows; i++){
+            vector<int> row;
+            for(int j = 0; j <= i; j++){
+                if(j == 0 || j == i){
+                    row.push_back(1);
+                }
+                else{
+                    row.push_back(triangle[i-1][j-1] + triangle[i-1][j]);
+                }
+            }
+            triangle.push_back(row);
+        }
+        return triangle;
+    }
+};
