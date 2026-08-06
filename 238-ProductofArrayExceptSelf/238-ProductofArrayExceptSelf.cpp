a9e4af1eadc85d29afd61ec18277d94cf110ec9b
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-08-06 20:31:43 +0530

    Create 238-ProductofArrayExceptSelf

---
+++ 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.cpp
@@ -0,0 +1,22 @@
+// Last updated: 06/08/2026, 20:31:42
+1class Solution {
+2public:
+3    vector<int> productExceptSelf(vector<int>& nums) {
+4        int n = nums.size();
+5        vector<int> left(n);
+6        vector<int> right(n);
+7        vector<int> ans(n);
+8        left[0] = 1;
+9        for(int i = 1; i < n ; i++){
+10            left[i] = left[i - 1] * nums[i - 1];
+11        }
+12        right[n - 1] = 1;
+13        for(int i = n - 2; i >= 0; i--){
+14            right[i] = right[i + 1] * nums[i + 1];
+15        }
+16        for(int i = 0; i < n ; i++){
+17            ans[i] = left[i] * right[i];
+18        }
+19        return ans;
+20    }
+21};
