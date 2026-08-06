3a0a88a928f493a45a9efffbb91cfe8f0c7b2330
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-08-06 21:01:26 +0530

    File updated at 06/08/2026, 21:01:26

--- 42-TrappingRainWater/42-TrappingRainWater.cpp
+++ 42-TrappingRainWater/42-TrappingRainWater.cpp
@@ -1,26 +1,26 @@
-// Last updated: 01/03/2026, 20:23:11
-class Solution {
-public:
-    int trap(vector<int>& height) {
-        int l = 0, r = height.size() - 1;
-        int leftMax = 0, rightMax = 0;
-        int water = 0;
-
-        while (l < r) {
-            if (height[l] <= height[r]) {
-                if (height[l] >= leftMax)
-                    leftMax = height[l];
-                else
-                    water += leftMax - height[l];
-                l++;
-            } else {
-                if (height[r] >= rightMax)
-                    rightMax = height[r];
-                else
-                    water += rightMax - height[r];
-                r--;
-            }
-        }
-        return water;
-    }
-};
+// Last updated: 06/08/2026, 21:01:26
+1class Solution {
+2public:
+3    int trap(vector<int>& height) {
+4        int l = 0, r = height.size() - 1;
+5        int leftMax = 0, rightMax = 0;
+6        int water = 0;
+7
+8        while (l < r) {
+9            if (height[l] <= height[r]) {
+10                if (height[l] >= leftMax)
+11                    leftMax = height[l];
+12                else
+13                    water += leftMax - height[l];
+14                l++;
+15            } else {
+16                if (height[r] >= rightMax)
+17                    rightMax = height[r];
+18                else
+19                    water += rightMax - height[r];
+20                r--;
+21            }
+22        }
+23        return water;
+24    }
+25};
