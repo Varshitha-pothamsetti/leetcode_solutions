6184361b8a72fb9b962a52ec901d4e9f82fc525e
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-08-15 13:55:01 +0530

    Create 287-FindtheDuplicateNumber

---
+++ 287-FindtheDuplicateNumber/287-FindtheDuplicateNumber.cpp
@@ -0,0 +1,15 @@
+// Last updated: 15/08/2026, 13:55:00
+1class Solution {
+2public:
+3    int findDuplicate(vector<int>& nums) {
+4        int arr = 0;
+5        sort(nums.begin(), nums.end());
+6        for(int i = 0 ; i < nums.size()-1;i++){
+7            if(nums[i] == nums[i+1]){
+8                arr = nums[i];
+9                break;
+10            }
+11        }
+12        return arr;
+13    }
+14};
