3625ebdb0b38b58cf52de2260163d7649856b7a0
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-08-17 19:57:55 +0530

    File updated at 17/08/2026, 19:57:55

--- 94-BinaryTreeInorderTraversal/94-BinaryTreeInorderTraversal.cpp
+++ 94-BinaryTreeInorderTraversal/94-BinaryTreeInorderTraversal.cpp
@@ -1,28 +1,31 @@
-// Last updated: 01/03/2026, 20:22:54
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
- void in(TreeNode *root,vector<int>&resinorder){
-  if(!root){
-  return ;
-  }
-  in(root->left,resinorder);
-  resinorder.push_back(root->val);
-  in(root->right,resinorder);
-}
- vector<int> inorderTraversal(TreeNode* root) {
-  vector<int>resinorder;
-  in(root,resinorder);
-  return resinorder;
-    }
-};
+// Last updated: 17/08/2026, 19:57:55
+1/**
+2 * Definition for a binary tree node.
+3 * struct TreeNode {
+4 *     int val;
+5 *     TreeNode *left;
+6 *     TreeNode *right;
+7 *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+8 *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+10 * };
+11 */
+12class Solution {
+13public:
+14    vector<int> inorderTraversal(TreeNode* root) {
+15        vector<int> ans;
+16        stack<TreeNode*> st;
+17        TreeNode* curr = root;
+18        while (curr != nullptr || !st.empty()) {
+19            while (curr != nullptr) {
+20                st.push(curr);
+21                curr = curr->left;
+22            }
+23            curr = st.top();
+24            st.pop();
+25            ans.push_back(curr->val);
+26            curr = curr->right;
+27        }
+28        return ans;
+29    }
+30};
