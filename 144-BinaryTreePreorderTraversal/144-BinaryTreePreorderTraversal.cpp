23bb9c81486a1bc864c1910cbc9066adbccf7135
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-08-19 20:22:55 +0530

    File updated at 19/08/2026, 20:22:54

--- 144-BinaryTreePreorderTraversal/144-BinaryTreePreorderTraversal.cpp
+++ 144-BinaryTreePreorderTraversal/144-BinaryTreePreorderTraversal.cpp
@@ -1,18 +1,29 @@
-// Last updated: 01/03/2026, 20:22:44
-class Solution {
-public:
- void Pre(TreeNode* root,vector<int>&resPreOrder)
-{
-  if(!root){
-      return ;
-  }
-  resPreOrder.push_back(root->val);
-  Pre(root->left,resPreOrder);
-  Pre(root->right,resPreOrder);
-}
-    vector<int> preorderTraversal(TreeNode* root) {
-    vector<int>resPreOrder;
-    Pre(root,resPreOrder);
-    return resPreOrder;
-}
-};
+// Last updated: 19/08/2026, 20:22:54
+1/**
+2 * Definition for a binary tree node.
+3 * struct TreeNode {
+4 *     int val;
+5 *     TreeNode *left;
+6 *     TreeNode *right;
+7 *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+8 *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+10 * };
+11 */
+12class Solution {
+13public:
+14 void Pre(TreeNode* root,vector<int>&resPreOrder)
+15{
+16  if(!root){
+17      return ;
+18  }
+19  resPreOrder.push_back(root->val);
+20  Pre(root->left,resPreOrder);
+21  Pre(root->right,resPreOrder);
+22}
+23    vector<int> preorderTraversal(TreeNode* root) {
+24    vector<int>resPreOrder;
+25    Pre(root,resPreOrder);
+26    return resPreOrder;
+27}
+28};
