595607fbe8e757f0d7e838db280ffe078e6ac1d5
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-09-09 21:25:48 +0530

    Create 2682-FindTheLosersOfTheCircularGame

---
+++ 2682-FindTheLosersOfTheCircularGame/2682-FindTheLosersOfTheCircularGame.cpp
@@ -0,0 +1,21 @@
+// Last updated: 09/09/2026, 21:25:48
+class Solution {
+public:
+    vector<int> circularGameLosers(int n, int k) {
+        vector<bool> received(n, false);
+        int current_friend = 0;
+        int turn = 1;
+        while(!received[current_friend]){
+            received[current_friend] = true;
+            current_friend = (current_friend + turn * k) % n;
+            turn++;
+        }
+        vector<int> losers;
+        for(int i = 0; i < n; i++){
+            if(!received[i]){
+                losers.push_back(i + 1);
+            }
+        }
+        return losers;
+    }
+};
