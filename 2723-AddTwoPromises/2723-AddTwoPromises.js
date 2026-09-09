e4537b4ed569d33c69117234828d6fc6be227178
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-09-09 21:25:09 +0530

    Create 2723-AddTwoPromises

---
+++ 2723-AddTwoPromises/2723-AddTwoPromises.js
@@ -0,0 +1,15 @@
+// Last updated: 09/09/2026, 21:25:08
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+    const [a, b] = await Promise.all([promise1, promise2]);
+    return a + b;
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
