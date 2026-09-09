f99e001033db74141d00be2200ff536766e9d5e9
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-09-09 21:25:18 +0530

    Create 2703-ReturnLengthOfArgumentsPassed

---
+++ 2703-ReturnLengthOfArgumentsPassed/2703-ReturnLengthOfArgumentsPassed.js
@@ -0,0 +1,12 @@
+// Last updated: 09/09/2026, 21:25:18
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    return args.length;
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
