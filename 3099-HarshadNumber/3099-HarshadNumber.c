1d85d57c191f22ca20adab9063aaa853c9ead6cb
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-09-09 21:24:40 +0530

    Create 3099-HarshadNumber

---
+++ 3099-HarshadNumber/3099-HarshadNumber.c
@@ -0,0 +1,13 @@
+// Last updated: 09/09/2026, 21:24:40
+int sumOfTheDigitsOfHarshadNumber(int x) {
+    int sum = 0;
+    int temp = x;
+    while(temp > 0){
+        sum += (temp % 10);
+        temp /= 10;
+    }
+        if(x % sum == 0){
+            return sum;
+        }
+         return -1;
+}
