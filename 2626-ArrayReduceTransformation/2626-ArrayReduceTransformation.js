4110b0579fe3dc940688d74a40eb1ce7decfaef8
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-09-09 21:26:04 +0530

    Create 2626-ArrayReduceTransformation

---
+++ 2626-ArrayReduceTransformation/2626-ArrayReduceTransformation.js
@@ -0,0 +1,14 @@
+// Last updated: 09/09/2026, 21:26:03
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    let val = init;
+    for(let i = 0 ; i < nums.length; i++){
+        val = fn(val, nums[i]);
+    }
+    return val;
+};
