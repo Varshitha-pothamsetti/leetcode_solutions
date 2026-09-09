bd9e8198daffb7275424a990f454108775b484f5
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-09-09 21:22:18 +0530

    Create 3870-CountCommasInRange

---
+++ 3870-CountCommasInRange/3870-CountCommasInRange.cpp
@@ -0,0 +1,15 @@
+// Last updated: 09/09/2026, 21:22:18
+class Solution {
+public:
+    int countCommas(int n) {
+        int ans = 0;
+        for(int i = 1000; i <= n; i++){
+            int x = i;
+            while(x >= 1000){
+                ans++;
+                x /= 1000;
+            }
+        }
+        return ans;
+    }
+};
