abb3f671b289e33909839d27fd000093656b8dc3
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-09-09 21:23:12 +0530

    Create 3432-CountPartitionsWithEvenSumDifference

---
+++ 3432-CountPartitionsWithEvenSumDifference/3432-CountPartitionsWithEvenSumDifference.cpp
@@ -0,0 +1,22 @@
+// Last updated: 09/09/2026, 21:23:11
+class Solution {
+public:
+    int countPartitions(vector<int>& nums) {
+        int n = nums.size();
+        int totalsum = 0;
+        for(int num : nums) totalsum += num;
+
+         int leftsum = 0;
+         int count = 0;
+
+         for(int i = 0; i < n - 1; i++){
+            leftsum = nums[i];
+            int rightsum = totalsum - leftsum;
+
+            if((leftsum % 2) == (rightsum % 2)){
+                count++;
+            }
+         }
+         return count;
+    }
+};
