c0d4d84ddfa64850eae5d9dbcb9b66c53658cf54
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-09-09 21:25:28 +0530

    Create 2667-CreateHelloWorldFunction

---
+++ 2667-CreateHelloWorldFunction/2667-CreateHelloWorldFunction.js
@@ -0,0 +1,15 @@
+// Last updated: 09/09/2026, 21:25:27
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World"
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
