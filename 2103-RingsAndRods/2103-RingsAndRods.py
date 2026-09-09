fd7b910edbb67ceff6088b52176c42d86f39c931
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-09-09 21:27:32 +0530

    Create 2103-RingsAndRods

---
+++ 2103-RingsAndRods/2103-RingsAndRods.py
@@ -0,0 +1,10 @@
+# Last updated: 09/09/2026, 21:27:32
+class Solution:
+    def countPoints(self, rings: str) -> int:
+        count = 0
+        for i in range(10):
+            c= str(i)
+            if "B"+c in rings and "G"+c in rings and "R"+c in rings:
+                count += 1
+        return count
+        
