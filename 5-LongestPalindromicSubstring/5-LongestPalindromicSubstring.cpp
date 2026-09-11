ec80fe4dad32c8c21286804f8ae70ec661601370
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-09-11 21:09:46 +0530

    File updated at 11/09/2026, 21:09:45

--- 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.cpp
+++ 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.cpp
@@ -1,37 +1,34 @@
-// Last updated: 06/09/2026, 21:33:01
+// Last updated: 11/09/2026, 21:09:45
 1class Solution {
 2public:
 3    string longestPalindrome(string s) {
 4        int start = 0;
 5        int maxLen = 1;
-6
-7        for (int i = 0; i < s.length(); i++) {
-8            int left = i, right = i;
-9
-10            while (left >= 0 && right < s.length() &&
-11                   s[left] == s[right]) {
-12                if (right - left + 1 > maxLen) {
-13                    start = left;
-14                    maxLen = right - left + 1;
-15                }
-16                left--;
-17                right++;
-18            }
+6        for (int i = 0; i < s.length(); i++) {
+7            int left = i, right = i;
+8            while (left >= 0 && right < s.length() &&
+9                   s[left] == s[right]) {
+10                if (right - left + 1 > maxLen) {
+11                    start = left;
+12                    maxLen = right - left + 1;
+13                }
+14                left--;
+15                right++;
+16            }
+17            left = i;
+18            right = i + 1;
 19
-20            left = i;
-21            right = i + 1;
-22
-23            while (left >= 0 && right < s.length() &&
-24                   s[left] == s[right]) {
-25                if (right - left + 1 > maxLen) {
-26                    start = left;
-27                    maxLen = right - left + 1;
-28                }
-29                left--;
-30                right++;
-31            }
-32        }
-33
-34        return s.substr(start, maxLen);
-35    }
-36};
+20            while (left >= 0 && right < s.length() &&
+21                   s[left] == s[right]) {
+22                if (right - left + 1 > maxLen) {
+23                    start = left;
+24                    maxLen = right - left + 1;
+25                }
+26                left--;
+27                right++;
+28            }
+29        }
+30
+31        return s.substr(start, maxLen);
+32    }
+33};
