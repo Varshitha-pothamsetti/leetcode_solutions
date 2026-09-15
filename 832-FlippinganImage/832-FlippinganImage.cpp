62d23313885e2e682fd923d740e1514225923ed7
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-09-15 20:28:14 +0530

    Create 832-FlippinganImage

---
+++ 832-FlippinganImage/832-FlippinganImage.cpp
@@ -0,0 +1,13 @@
+// Last updated: 15/09/2026, 20:28:13
+1class Solution {
+2public:
+3    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
+4        for(int i = 0; i < image.size(); i++){
+5            reverse(image[i].begin(), image[i].end());
+6            for(int j = 0; j < image.size(); j++){
+7                image[i][j] = 1 - image[i][j];
+8            }
+9        }
+10        return image;
+11    }
+12};
