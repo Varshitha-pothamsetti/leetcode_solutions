317d11c3c8af8e47368ebab7372772068594d53b
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-09-18 14:29:42 +0530

    Create 3731-FindMissingElements

---
+++ 3731-FindMissingElements/3731-FindMissingElements.cpp
@@ -0,0 +1,15 @@
+// Last updated: 18/09/2026, 14:29:40
+1class Solution {
+2public:
+3    vector<int> findMissingElements(vector<int>& nums) {
+4        int min = *min_element(nums.begin(), nums.end());
+5        int max = *max_element(nums.begin(), nums.end());
+6        vector<int> ans;
+7        for(int i = min; i <= max; i++){
+8            if(find(nums.begin(), nums.end(), i) == nums.end()){
+9                ans.push_back(i);
+10            }
+11        }
+12        return ans;
+13    }
+14};
