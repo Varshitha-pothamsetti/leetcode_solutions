012e88f50f19c007c41ef7ae9491062a04f5e5a4
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-09-23 20:54:51 +0530

    Create 783-MinimumDistanceBetweenBSTNodes

---
+++ 783-MinimumDistanceBetweenBSTNodes/783-MinimumDistanceBetweenBSTNodes.cpp
@@ -0,0 +1,30 @@
+// Last updated: 23/09/2026, 20:54:51
+1/**
+2 * Definition for a binary tree node.
+3 * struct TreeNode {
+4 *     int val;
+5 *     TreeNode *left;
+6 *     TreeNode *right;
+7 *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+8 *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+10 * };
+11 */
+12class Solution {
+13public:
+14    int ans = INT_MAX;
+15    int prev = -1;
+16    void inorder(TreeNode* root){
+17        if(root == NULL)
+18            return;
+19        inorder(root -> left);
+20        if(prev != -1)
+21           ans = min(ans, root -> val - prev);
+22           prev = root -> val;
+23           inorder(root -> right);
+24    }
+25    int minDiffInBST(TreeNode* root) {
+26        inorder(root);
+27        return ans;
+28    }
+29};
